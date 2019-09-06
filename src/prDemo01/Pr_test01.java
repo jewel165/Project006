9b85ae0c462243bc3adb07d5436216420ec435ac
jewel165 <54503644+jewel165@users.noreply.github.com>
2019-09-05 20:31:08 -0400

    just started
    first push on this project

---
+++ src/prDemo01/Pr_test01.java
@@ -0,0 +1,18 @@
+package prDemo01;
+
+public class Pr_test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//this is a test
+		
+		
+		 String rr = "md";
+		 String tt = "alam";
+				
+	}
+
+}
